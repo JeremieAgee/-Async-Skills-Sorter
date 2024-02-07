d74654f7e7f3772d433ea6e15b02c5cb641d13ad
JeremieAgee <you@example.com>
2024-02-06 21:29:36 -0600

    Updated

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 'use strict';
+let response = [];
 async function getData(){
     try{
-        const response = await (await fetch('skills.json')).json();
+        response = await (await fetch('skills.json')).json();
         response.sort((a, b) => b.level - a.level);
         console.log(response);
 
@@ -12,3 +13,15 @@ async function getData(){
     
 }
 getData();
+function displayResponse(){
+    const table = document.getElementById('table-body');
+    table.innerHTML = "";
+    for (let i=0; i<response.length; i++){
+        const tr = document.createElement('tr');
+        tr.innerHTML=`<tr><td>${response[i].skillName}</td><td>${response[i].level}</td></tr>
+        `;
+        table.appendChild(tr);
+        console.log(response[i].skillName);
+    };  
+}
+displayResponse();
