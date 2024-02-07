d919343b50b36775a7a4035c860cb86a8c747cf8
JeremieAgee <you@example.com>
2024-02-06 21:39:48 -0600

    FIxed issues

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ async function getData(){
         response = await (await fetch('skills.json')).json();
         response.sort((a, b) => b.level - a.level);
         console.log(response);
+        const table = document.getElementById('table-body');
+        table.innerHTML = "";
+        for(let i=0; i<response.length; i++){
+            const tr = document.createElement('tr');
+            tr.innerHTML=`<tr><td>${response[i].skillName}</td><td>${response[i].level}</td></tr>`;
+            table.appendChild(tr);
+        };
 
     } catch (error){
         console.log(error);
@@ -13,15 +20,19 @@ async function getData(){
     
 }
 getData();
-function displayResponse(){
+async function displayResponse(){
+    try{
     const table = document.getElementById('table-body');
     table.innerHTML = "";
-    for (let i=0; i<response.length; i++){
+     for(let i=0; i<response.length-1; i++){
         const tr = document.createElement('tr');
         tr.innerHTML=`<tr><td>${response[i].skillName}</td><td>${response[i].level}</td></tr>
         `;
         table.appendChild(tr);
         console.log(response[i].skillName);
-    };  
+    }; } catch (error){
+        console.log(error);
+        return null;
+    }
 }
 displayResponse();
